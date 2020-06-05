d9ec247fe85fb506ff89209ad56739cebe31a810
ikzsl <ikzsl@yandex.ru>
2020-06-05 11:06:54 +0300

    Updates

---
+++ precache-manifest.e2747e97ffdbe7d04c6189d6162c60fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19fd2c6fa04530093e2e5871adaf2cbb",
+    "url": "/blog-form/index.html"
+  },
+  {
+    "revision": "7771e53a43d6ffba769c",
+    "url": "/blog-form/static/css/2.4deadad5.chunk.css"
+  },
+  {
+    "revision": "25d0e98a14de48f9cb95",
+    "url": "/blog-form/static/css/main.c523563d.chunk.css"
+  },
+  {
+    "revision": "7771e53a43d6ffba769c",
+    "url": "/blog-form/static/js/2.3cabf42b.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/blog-form/static/js/2.3cabf42b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25d0e98a14de48f9cb95",
+    "url": "/blog-form/static/js/main.9c2e5a74.chunk.js"
+  },
+  {
+    "revision": "948dfe8fcb84d4b1dbf6",
+    "url": "/blog-form/static/js/runtime-main.0dbf890a.js"
+  }
+]);
