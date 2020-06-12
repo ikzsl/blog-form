d20dedafafd95826d27a608b0692d4a8a7dbf96b
ikzsl <ikzsl@yandex.ru>
2020-06-12 19:19:40 +0300

    fix Redux DevTools bug

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import { createStore, applyMiddleware, compose } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 
 import reducers from './reducers/reducers';
 
@@ -12,7 +12,7 @@ import 'antd/dist/antd.css';
 import './index.scss';
 import App from './components/app/App';
 
-const store = createStore(reducers, compose(applyMiddleware(thunk), composeWithDevTools()));
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
